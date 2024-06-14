58029f255dcf7c902982c1138c8c6d3c0188a089
Hermes1030 <59109940+Hermes1030@users.noreply.github.com>
2024-06-14 08:44:41 +0800

    add

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=75)
     body = models.TextField()
+    # 通常是一个用于生成友好URL的短标签或别名
     slug = models.SlugField()
     datetime = models.DateTimeField(auto_now_add=True)
 
